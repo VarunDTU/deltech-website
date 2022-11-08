77e197b67eadc38cdd3a6e0a2056e0d3427f8788
Vaibhav <vaibhav2438@gmail.com>
2022-11-08 12:18:53 +0530

    Update faqs.js

--- src/faqs.js
+++ src/faqs.js
@@ -6,17 +6,17 @@ const Faqs = () => {
       <div className="container w-10/12 mx-auto flex flex-col px-5 py-24 justify-center items-center">
         <h1 className="text-5xl mb-16 font-bold">FAQ&#39;S</h1>
         <div className="mb-5 text-md sm:text-lg">
-          <p>What is AISYWLC?</p>
+          <p>What is DELTECH-MUN?</p>
           <p className="text-xs leading-relaxed sm:text-lg">
-            The flagship event of IEEE India Council, IEEE AISYWLC has always
+            The flagship event of XYZ India Council, XYZ DELTECH-MUN has always
             been a colloquium that brings together youth with professionals and
             experts with their ideologies tallying up to be the best exchange of
-            wisdom and acuity. Founded in 2000, the AISYWLC has grown over the
-            years to become the biggest event on the calendar for IEEE members
+            wisdom and acuity. Founded in 2000, the DELTECH-MUN has grown over the
+            years to become the biggest event on the calendar for XYZ members
             in India. It is a conglomeration of inventors, professionals,
             entrepreneurs, visionaries and some of the greatest minds in the
             country. By bringing together amazing people from all walks of life,
-            the AISYWLC empowers its participants to be inspired, collaborate
+            the DELTECH-MUN empowers its participants to be inspired, collaborate
             and innovate for tomorrow.
             <br />
             <br />
@@ -31,8 +31,8 @@ const Faqs = () => {
             celebratory and an inspirational session for the women who've made
             it successfully and created benchmarks that we all look up to.
           </p>
-          <p>Where will AISYWLC 2020 be held?</p>
-          <p className="text-xs leading-relaxed sm:text-lg">AISYWLC 2020 is conducted across the India in Hybrid mode.</p>
+          <p>Where will DELTECH-MUN 2022 be held?</p>
+          <p className="text-xs leading-relaxed sm:text-lg">DELTECH-MUN 2020 is conducted across the India in Hybrid mode.</p>
           <p>What is the Congress theme?</p>
           <p className="text-xs leading-relaxed sm:text-lg">
             The theme revolves around how Indian culture, sciences and methods
@@ -42,13 +42,13 @@ const Faqs = () => {
           </p>
           <p>Where can I find out what is happening at the Congress?</p>
           <p className="text-xs leading-relaxed sm:text-lg">
-            See the program section on the AISYWLC 2020 website
-            http://aisywc.in/. A schedule of events will also be shared with
+            See the program section on the DELTECH-MUN 2022 website
+            http://deltechmun.in/. A schedule of events will also be shared with
             you.
           </p>
           <p>Which events can I attend?</p>
           <p className="text-xs leading-relaxed sm:text-lg">
-            Anyone who has registered for AISYWLC 2020 and has paid the Congress
+            Anyone who has registered for DELTECH-MUN 2022 and has paid the Congress
             fee may attend all the events listed in schedule.
           </p>
         </div>
