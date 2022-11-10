1c6834e12720566dbed5ad70d5d2eeff0a9bf8b3
Vaibhav <vaibhav2438@gmail.com>
2022-11-10 14:43:17 +0530

    removing extra width

--- src/components/main.js
+++ src/components/main.js
@@ -5,6 +5,7 @@ const Main = () => {
       <section className="text-gray-600 -z-50 w-full" id="home">
         <div className="mx-0 content-center">
           <div className="relative w-full h-screen ">
+<<<<<<< Updated upstream
             <div className="w-full h-full bg-[url('./images/landing_notext.png')] bg-cover bg-center bg-fixed backdrop-blur-3xl mix-blend-darken">
               <div className="content-center flex flex-col h-screen justify-center items-center">
                 <h1 className="mt-5 text-center md:text-7xl text-5xl text-white bg-black/70 backdrop-blur-sm font-semibold drop-shadow-lg py-2 border-black mix-blend-darken">
@@ -17,6 +18,25 @@ const Main = () => {
                   <button className="md:inline-flex border-0 py-1 px-3 mt-2 focus:outline-none bg-white hover:outline-2 outline hover:bg-transparent hover:text-black rounded text-base mx-2 ease-linear transition-all">
                     Know More
                   </button>
+=======
+            <div className="w-full h-full bg-[url('landing_notext.png')] bg-cover bg-center backdrop-blur-3xl mix-blend-darken">
+              <div className="content-center flex flex-col h-screen justify-center items-center">
+                <h1 className="mt-5 text-center lg:text-6xl  sm:text-5xl text-4xl text-white bg-black/70 backdrop-blur-sm font-semibold drop-shadow-lg py-2 border-black mix-blend-darken">
+                  DELTECH MODEL UNITED NATIONS CONFERENCE 2023
+                </h1>
+                <div className="py-5 justify-center flex md:flex-row flex-col z-5">
+                  <a href="#_" className="relative inline-block text-lg group">
+                    <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                      <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                      <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                      <span className="relative">Explore</span>
+                    </span>
+                    <span
+                      className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                      data-rounded="rounded-lg"
+                    ></span>
+                  </a>
+>>>>>>> Stashed changes
                 </div>
               </div>
             </div>
--- src/components/newnavbar.js
+++ src/components/newnavbar.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React, { useState } from "react";
 import { Transition } from "@headlessui/react";
 import {Link} from 'react-scroll';
@@ -124,3 +125,127 @@ function Nav() {
 }
 
 export default Nav;
+=======
+import { useState } from "react";
+import { Link } from "react-scroll";
+export default function NavBar2() {
+  const [navbar, setNavbar] = useState(false);
+
+  return (
+    <nav className="w-full bg-gray-800 shadow z-10 text-white fixed">
+      <div className="justify-between px-4 mx-auto lg:max-w-7xl lg:items-center lg:flex lg:px-8">
+        <div>
+          <div className="flex items-center justify-between py-3 lg:py-3 lg:block">
+            <a href="/" className="flex title-font font-medium items-center mb-4">
+              <img
+                alt="/"
+                height="50"
+                className="h-8 pr-1 mx-auto"
+                src="/images/whiteDelTech.png"
+              ></img>
+              <span className="ml-3 text-xl">DelTech MUN</span>
+            </a>
+            <div className="lg:hidden">
+              <button
+                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                onClick={() => setNavbar(!navbar)}
+              >
+                {navbar ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 lg:block lg:pb-0 lg:mt-0 ${
+              navbar ? "block" : "hidden"
+            }`}
+          >
+            <ul className="items-center justify-center space-y-8 lg:flex lg:space-x-6 lg:space-y-0">
+              <li className="text-gray-600 hover:text-blue-600">
+                <Link
+                  activeClass="active"
+                  className="mr-2 hover:text-yellow-600"
+                  to="home"
+                  smooth={true}
+                  duration={500}
+                >
+                  HOME
+                </Link>
+              </li>
+              <li className="text-gray-600 hover:text-blue-600">
+                <Link
+                  activeClass="active"
+                  className="mr-2 hover:text-red-600"
+                  to="temp"
+                  smooth={true}
+                  duration={500}
+                >
+                  ABOUT
+                </Link>
+              </li>
+              <li className="text-gray-600 hover:text-blue-600">
+                <Link
+                  activeClass="active"
+                  className="mr-2 hover:text-red-600"
+                  to="contacts"
+                  smooth={true}
+                  offset={50}
+                  duration={500}
+                >
+                  CONTACT
+                </Link>
+              </li>
+              <li className="text-gray-600 hover:text-blue-600">
+                <Link
+                  activeClass="active"
+                  className="mr-2 hover:text-red-600"
+                  to="contacts"
+                  smooth={true}
+                  offset={50}
+                  duration={500}
+                >
+                  COMMITTEES
+                </Link>
+              </li>
+              <li className="text-gray-600 hover:text-blue-600">
+                <button className="inline-flex border-0 py-1 px-5 focus:outline-none bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 rounded text-base">
+                  Register Now
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+>>>>>>> Stashed changes
