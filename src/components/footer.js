145f7eeb329d3e4bf98dd0daa7399d72e53f95ae
Vaibhav <vaibhav2438@gmail.com>
2022-11-12 01:05:43 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -15,7 +15,7 @@ const Footer = () => {
             referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
-        <div className=" text-white p-10 bg-cyan-800 w-full ">
+        <div className=" text-white p-10 bg-slate-700 w-full ">
           <h1 className="text-4xl lg:text-5xl font-bold mt-5 text-center">
             Venue
           </h1>
@@ -26,46 +26,52 @@ const Footer = () => {
         </div>
       </div>
 
-      <footer className=" bg-white sm:p-6 dark:bg-gray-900 w-full">
-        <div className="pt-4 w-full flex items-center flex-col lg:flex-row justify-evenly">
+      <footer className=" bg-white dark:bg-gray-900 p-10 w-full">
+        <div className="md:pt-10 w-full flex items-center flex-col lg:flex-row justify-evenly">
           <div className="flex flex-col items-center mb-6 ml-4 md:mb-0">
-            <div className="self-center mb-5 text-2xl font-semibold whitespace-nowrap dark:text-white">
-              For any queries contact :
+            <div className="self-center mb-8 text-lg md:text-2xl  font-semibold whitespace-nowrap dark:text-white">
+              For Any Queries Contact :
             </div>
-            <div className="flex flex-col justify-evenly items-center md:flex-row lg:flex-col">
-              <div className="p-4 w-72 mb-5 lg:mr-0 md:mr-5 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <h5 className="mb-2 text-lg font-bold text-gray-900 dark:text-white">
+            <div className="flex flex-col justify-evenly items-center md:flex-row ">
+              <div className="p-4 w-72 mb-5 flex flex-col justify-evenly items-center md:mr-5 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <p className="text-gray-500 text-xs text-center font-normal pb-2">
+                  President
+                </p>
+                <h5 className="mb-2  md:text-lg font-bold text-gray-900 dark:text-white">
                   Sunny
                 </h5>
-                <div className="font-normal flex flex-row text-gray-700 dark:text-gray-400">
+                <div className="font-normal flex flex-row items-center text-gray-700 dark:text-gray-400">
                   <span>
                     <FaPhoneAlt />
                   </span>
-                  <span className="ml-4 -translate-y-1">1 2 ka 4</span>
+                  <span className="ml-3 text-sm ">1 2 ka 4</span>
                 </div>
               </div>
-              <div className="p-4 w-72 mb-5 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <h5 className="mb-2 text-lg font-bold text-gray-900 dark:text-white">
+              <div className="p-4 w-72 mb-5 bg-white flex flex-col justify-evenly items-center rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <p className="text-gray-500 text-xs text-center font-normal pb-2">
+                  Vice President
+                </p>
+                <h5 className="mb-2 md:text-lg font-bold text-gray-900 dark:text-white">
                   Munny
                 </h5>
-                <div className="font-normal flex flex-row text-gray-700 dark:text-gray-400">
+                <div className="font-normal flex flex-row items-center text-gray-700 dark:text-gray-400">
                   <span>
                     <FaPhoneAlt />
                   </span>
-                  <span className="ml-4 -translate-y-1">4 2 ka 1</span>
+                  <span className="ml-3 text-sm">4 2 ka 1</span>
                 </div>
               </div>
             </div>
           </div>
-          <div className="p-10 w-72 mb-5">
-            <h5 className="mb-2 text-lg font-bold text-gray-900 dark:text-white">
+          <div className="md:px-10 w-72 mb-5 flex flex-col items-center">
+            <h5 className="mb-2 md:text-lg font-bold text-gray-900 dark:text-white">
               Get In TOUCH
             </h5>
-            <div className="font-normal flex flex-row text-gray-700 dark:text-gray-400">
+            <div className="font-normal items-center flex flex-row text-gray-700 dark:text-gray-400">
               <span>
                 <FaEnvelope />
               </span>
-              <span className="ml-4 -translate-y-1">Email</span>
+              <span className="ml-2 text-sm ">Lorem ipsum dolor</span>
             </div>
           </div>
         </div>
@@ -125,9 +131,13 @@ const Footer = () => {
                 viewBox="0 0 24 24"
                 aria-hidden="true"
               >
-                <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
+                <path
+                  fill-rule="evenodd"
+                  d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"
+                  clip-rule="evenodd"
+                ></path>
               </svg>
-              <span className="sr-only">Twitter page</span>
+              <span className="sr-only">LinkedIn</span>
             </a>
             <a
               href="/"
@@ -139,13 +149,9 @@ const Footer = () => {
                 viewBox="0 0 24 24"
                 aria-hidden="true"
               >
-                <path
-                  fill-rule="evenodd"
-                  d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                  clip-rule="evenodd"
-                ></path>
+                <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
               </svg>
-              <span className="sr-only">GitHub account</span>
+              <span className="sr-only">Twitter page</span>
             </a>
           </div>
         </div>
