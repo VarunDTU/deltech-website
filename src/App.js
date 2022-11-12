c5f96e6c539acd1c607fc81f5fa3b5f25bcd24e7
Vaibhav <vaibhav2438@gmail.com>
2022-11-12 10:03:02 +0530

    gallery

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Main from "./components/main";
 import About from "./components/about";
 import Faqs from "./components/faqs";
 import Committees from "./components/committees";
+import Gallery from "./components/gallery";
 import Footer from "./components/footer";
 import ScrollToTop from "react-scroll-to-top";
 // import Comingsoon from './components/comingsoon'
@@ -17,20 +18,21 @@ function App() {
       <ScrollToTop
         smooth
         color="white"
-        width="50px"
-        height="50px"
-        svgPath="M17.504 26.025l.001-14.287 6.366 6.367L26 15.979 15.997 5.975 6 15.971 8.129 18.1l6.366-6.368v14.291z"
-        viewBox="0 0 32 32" 
+        width="42px"
+        height="42px"
+        svgPath="M12.7,12.5C12.7,12.5,12.7,12.5,12.7,12.5c-0.4-0.4-1-0.4-1.4,0l-3,3c-0.4,0.4-0.4,1,0,1.4c0.4,0.4,1,0.4,1.4,0l2.3-2.3l2.3,2.3c0.2,0.2,0.4,0.3,0.7,0.3c0.3,0,0.5-0.1,0.7-0.3c0.4-0.4,0.4-1,0-1.4L12.7,12.5z M9.7,11.5L12,9.2l2.3,2.3c0.2,0.2,0.4,0.3,0.7,0.3c0.3,0,0.5-0.1,0.7-0.3c0.4-0.4,0.4-1,0-1.4l-3-3c0,0,0,0,0,0c-0.4-0.4-1-0.4-1.4,0l-3,3c-0.4,0.4-0.4,1,0,1.4C8.7,11.8,9.3,11.8,9.7,11.5z"
+        viewBox="0 0 24 24"
         style={{
           backgroundColor: "#9400D3",
           borderRadius: "40px",
-          boxShadow: "10px 5px 5px #800080",
+         
         }}
       />
 
       <About />
       <Committees />
-      <Council/>
+      <Council />
+      <Gallery />
       <Faqs />
       <Footer />
     </div>
