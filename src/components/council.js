a4c38feea9885aa63c88a55f5d0af856363d0634
Varun <varun.py10@gmail.com>
2022-11-12 10:44:09 +0530

    carousel center mode

--- src/components/council.js
+++ src/components/council.js
@@ -7,16 +7,16 @@ export default class Council extends Component {
   render() {
     var councilList=[
         {name:"Ishpreet.png",desi:'External affairs'},{name:"Kushi.png",desi:'Internal Affairs'},{name:"Lakshay.jpg",desi:'Public Relations'},{name:"Nilesh.jpg",desi:'Operations'},{name:"RahulSingh.jpg",desi:'Corporate Affairs'},{name:"RitikaRai.jpg",desi:'Publicity'},{name:"Saksham.jpg",desi:'Finance'},{name:"Sankalp.jpg",desi:'MUN'},{name:"Shruti.jpg",desi:'Social & Outreach'},{name:"Siddharth.jpg",desi:'Debating'},{name:"Tushar.jpg"
-,desi:'Research & Policy'},];
+,desi:'Research & Policy'},{name:"Rtanjay.jpg",desi:'Administrative Affairs'}];
     
-    var councilcc= councilList.map(item => <div class="flex justify-center">
-    <div class="rounded-2xl z-5 shadow-lg bg-gray-800 outline outline-2 outline-black py-5 max-w-sm px-3" >
+    var councilcc= councilList.map(item => <div class="flex justify-center py-10 px-5">
+    <div class="rounded-2xl z-5 shadow-lg bg-white outline outline-2 outline-black shadow-black py-5 max-w-sm px-3" >
       <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
         <img class="rounded-t-lg h-60 object-cover w-96" src={`/images/Council/${item.name}`} alt=""/>
       </a>
       <div class="p-6">
-        <h5 class="text-white text-xl font-medium mb-2">{item.name.slice(0,-4)}</h5>
-        <p class="text-gray-300 text-base mb-4">
+        <h5 class="text-black text-xl font-bold mb-2">{item.name.slice(0,-4)}</h5>
+        <p class="text-grey-700 text-base mb-4">
           {item.desi}
         </p>
       
@@ -26,10 +26,16 @@ export default class Council extends Component {
     var settings = {
       infinite: true,
       slidesToShow: 3,
-      slidesToScroll: 1,
+      focusOnSelect: true,
       autoplay: true,
+      slidesToScroll: 1,
       speed: 5000,
-      autoplaySpeed: 5000,
+      swipeToSlide: true,
+      centerMode: true,
+      pauseOnHover: true,
+      autoplaySpeed: 3000,
+      initialSlide:1,
+      dots:true,
       responsive: [
         {
           breakpoint: 1024,
@@ -72,6 +78,8 @@ export default class Council extends Component {
             {councilcc[7]}
             {councilcc[8]}
             {councilcc[9]}
+            {councilcc[10]}
+            {councilcc[11]}
            
 
         </Slider>
