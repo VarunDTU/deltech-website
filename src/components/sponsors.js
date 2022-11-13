be0eb5080128f37b0449119d08b80d6e55c2945f
Varun <varun.py10@gmail.com>
2022-11-13 20:34:06 +0530

    added sponsor images

--- src/components/sponsors.js
+++ src/components/sponsors.js
@@ -2,44 +2,80 @@ import React from 'react'
 
 const Sponsors = () => {
   return (
-    <section class="overflow-hidden text-gray-700 ">
-  <div class="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
-    <h1 className="text-5xl mb-16 font-bold text-center">SPONSORS</h1>
+    <section class="overflow-hidden text-gray-700">
+  <div class="container px-3 py-2 mx-auto lg:pt-12 lg:px-24">
+    <h1 className="text-5xl mb-16 font-bold text-center">PAST PARTNERS</h1>
     <div class="flex flex-wrap -m-1 md:-m-2">
-      <div class="flex flex-wrap w-1/3">
+      <div class="flex flex-wrap w-1/4">
         <div class="w-full p-1 md:p-2">
-          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
-            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"/>
+          <img alt="gallery" class="block object-fit object-center w-full h-full rounded-lg"
+            src="/images/sponsors/ai.png"/>
         </div>
       </div>
-      <div class="flex flex-wrap w-1/3">
+      <div class="flex flex-wrap w-1/4">
         <div class="w-full p-1 md:p-2">
-          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
-            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp"/>
+          <img alt="gallery" class="block object-fit object-center w-full h-full rounded-lg"
+            src="/images/sponsors/iasac.png"/>
         </div>
       </div>
-      <div class="flex flex-wrap w-1/3">
+      <div class="flex flex-wrap w-1/4">
         <div class="w-full p-1 md:p-2">
-          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
-            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp"/>
+          <img alt="gallery" class="block object-fit object-center w-full h-full rounded-lg"
+            src="/images/sponsors/deexp.png"/>
         </div>
       </div>
-      <div class="flex flex-wrap w-1/3">
+      <div class="flex flex-wrap w-1/4">
         <div class="w-full p-1 md:p-2">
-          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
-            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp"/>
+          <img alt="gallery" class="block object-fit object-center w-full h-full rounded-lg"
+            src="/images/sponsors/career.png"/>
         </div>
       </div>
-      <div class="flex flex-wrap w-1/3">
+      <div class="flex flex-wrap w-1/4">
         <div class="w-full p-1 md:p-2">
-          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
-            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(76).webp"/>
+          <img alt="gallery" class="block object-fit object-center w-full h-full rounded-lg"
+            src="/images/sponsors/campus.png"/>
         </div>
       </div>
-      <div class="flex flex-wrap w-1/3">
+      <div class="flex flex-wrap w-1/4">
         <div class="w-full p-1 md:p-2">
-          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
-            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"/>
+          <img alt="gallery" class="block object-fit object-center w-full h-full rounded-lg"
+            src="/images/sponsors/cafe.png"/>
+        </div>
+      </div>
+      <div class="flex flex-wrap w-1/4">
+        <div class="w-full p-1 md:p-2">
+          <img alt="gallery" class="block object-fit object-center w-full h-full rounded-lg"
+            src="/images/sponsors/ims.png"/>
+        </div>
+      </div>
+      <div class="flex flex-wrap w-1/4">
+        <div class="w-full p-1 md:p-2">
+          <img alt="gallery" class="block object-fit object-center w-full h-full rounded-lg"
+            src="/images/sponsors/powergrid.png"/>
+        </div>
+      </div>
+      <div class="flex flex-wrap w-1/4">
+        <div class="w-full p-1 md:p-2">
+          <img alt="gallery" class="block object-fit object-center w-full h-full rounded-lg"
+            src="/images/sponsors/siec.png"/>
+        </div>
+      </div>
+      <div class="flex flex-wrap w-1/4">
+        <div class="w-full p-1 md:p-2">
+          <img alt="gallery" class="block object-fit object-center w-full h-full rounded-lg"
+            src="/images/sponsors/robinhood.png"/>
+        </div>
+      </div>
+      <div class="flex flex-wrap w-1/4">
+        <div class="w-full p-1 md:p-2">
+          <img alt="gallery" class="block object-fit object-center w-full h-full rounded-lg"
+            src="/images/sponsors/levedu.png"/>
+        </div>
+      </div>
+      <div class="flex flex-wrap w-1/4">
+        <div class="w-full p-1 md:p-2">
+          <img alt="gallery" class="block object-fit object-center w-full h-full rounded-lg"
+            src="/images/sponsors/iasnal.png"/>
         </div>
       </div>
     </div>
