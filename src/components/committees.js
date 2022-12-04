19f78f278acfe585f435fe550ea671ca047f1864
Varun <varun.py10@gmail.com>
2022-12-04 19:54:41 +0530

    register button fixed

--- src/components/committees.js
+++ src/components/committees.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { FaTimes } from "react-icons/fa";
-
+import RegisterButton from "./registerbutton";
 const Committees = () => {
   return (
     <>
-      <section className="text-center">
+      <section className="text-center z-50 absolute">
         <div
           class="modal fade z-10 fixed top-0 left-0 hidden text-center w-full h-full outline-none overflow-x-hidden overflow-y-auto"
           id="disec"
@@ -76,19 +76,7 @@ const Committees = () => {
                 </div>
                 
                 <div className="w-full flex justify-center pt-5 pb-5"></div>
-                <a href="#_" class="relative inline-block text-sm group">
-                  <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
-                    <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-                    <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gradient-to-r from-blue-500 to-purple-500 group-hover:rotate-180 ease"></span>
-                    <span class="relative">Register Now</span>
-                    
-                  </span>
-                  
-                  <span
-                    class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-700 rounded-lg group-hover:mb-0 group-hover:mr-0"
-                    data-rounded="rounded-lg"
-                  ></span>
-                </a>
+                <RegisterButton></RegisterButton>
                 
               </div>
             </div>
@@ -162,18 +150,7 @@ const Committees = () => {
                     </div>
                   </div>
                 </div>
-                <div className="w-full flex justify-center pt-5 pb-5"></div>
-                <a href="#_" class="relative inline-block text-sm group">
-                  <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
-                    <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-                    <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gradient-to-r from-blue-500 to-purple-500 group-hover:rotate-180 ease"></span>
-                    <span class="relative">Register Now</span>
-                  </span>
-                  <span
-                    class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-700 rounded-lg group-hover:mb-0 group-hover:mr-0"
-                    data-rounded="rounded-lg"
-                  ></span>
-                </a>
+                <RegisterButton></RegisterButton>
               </div>
             </div>
           </div>
@@ -248,17 +225,7 @@ const Committees = () => {
                 </div>
 
                 <div className="w-full flex justify-center pt-5 pb-5"></div>
-                <a href="#_" class="relative inline-block text-sm group">
-                  <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
-                    <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-                    <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gradient-to-r from-blue-500 to-purple-500 group-hover:rotate-180 ease"></span>
-                    <span class="relative">Register Now</span>
-                  </span>
-                  <span
-                    class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-700 rounded-lg group-hover:mb-0 group-hover:mr-0"
-                    data-rounded="rounded-lg"
-                  ></span>
-                </a>
+                <RegisterButton></RegisterButton>
               </div>
             </div>
           </div>
@@ -332,17 +299,7 @@ const Committees = () => {
               </div>
 
               <div className="modal-body relative p-4">
-                <a href="#_" class="relative inline-block text-sm group">
-                  <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
-                    <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-                    <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gradient-to-r from-blue-500 to-purple-500 group-hover:rotate-180 ease"></span>
-                    <span class="relative">Register Now</span>
-                  </span>
-                  <span
-                    class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-700 rounded-lg group-hover:mb-0 group-hover:mr-0"
-                    data-rounded="rounded-lg"
-                  ></span>
-                </a>
+              <RegisterButton></RegisterButton>
               </div>
             </div>
           </div>
@@ -415,17 +372,7 @@ const Committees = () => {
                 </div>
 
                 <div className="w-full flex justify-center pt-5 pb-5"></div>
-                <a href="#_" class="relative inline-block text-sm group">
-                  <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
-                    <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-                    <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gradient-to-r from-blue-500 to-purple-500 group-hover:rotate-180 ease"></span>
-                    <span class="relative">Register Now</span>
-                  </span>
-                  <span
-                    class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-700 rounded-lg group-hover:mb-0 group-hover:mr-0"
-                    data-rounded="rounded-lg"
-                  ></span>
-                </a>
+                <RegisterButton></RegisterButton>
               </div>
             </div>
           </div>
@@ -498,17 +445,7 @@ const Committees = () => {
                   </div>
                 </div>
                 <div className="w-full flex justify-center pt-5 pb-5"></div>
-                <a href="#_" class="relative inline-block text-sm group">
-                  <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
-                    <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-                    <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gradient-to-r from-blue-500 to-purple-500 group-hover:rotate-180 ease"></span>
-                    <span class="relative">Register Now</span>
-                  </span>
-                  <span
-                    class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-700 rounded-lg group-hover:mb-0 group-hover:mr-0"
-                    data-rounded="rounded-lg"
-                  ></span>
-                </a>
+                <RegisterButton></RegisterButton>
               </div>
             </div>
           </div>
--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Timer from "./timeer";
 import { Link } from "react-router-dom";
+import RegisterButton from "./registerbutton";
 const Main = () => {
   return (
     <div>
@@ -56,38 +57,7 @@ const Main = () => {
 
             </div>
               </Link>
-                <div className="peer py-5 justify-center flex md:flex-row flex-col z-5 mx-3">
-                  <a href="#_" className="relative inline-block text-lg group">
-                    <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
-                      <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-                      <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gradient-to-r from-blue-500 to-purple-500 group-hover:rotate-180 ease"></span>
-                      <span className="relative">Register Now</span>
-                    </span>
-                    <span
-                      className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-700 rounded-lg group-hover:mb-0 group-hover:mr-0"
-                      data-rounded="rounded-lg"
-                    ></span>
-                  </a>
-                </div>
-                {/* <!-- the menu here --> */}
-                <div className="hidden peer-hover:flex hover:flex rounded-lg flex-col bg-white drop-shadow-lg">
-                  <a
-                    className="px-5 py-3 hover:bg-blue-200"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://forms.gle/oZKzN6vKpyEtEgaC7"
-                  >
-                    Delegate Application
-                  </a>
-                  <a
-                    className="px-5 py-3 hover:bg-blue-200"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://forms.gle/aFJrRfyevoUmt7SS7"
-                  >
-                    IP Application
-                  </a>
-                </div>
+               <RegisterButton></RegisterButton>
               </div>
             </div>
           </div>
